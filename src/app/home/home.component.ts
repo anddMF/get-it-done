4419e5f89e2dbe3c67233a5fb8f055ccf1bca107
Andrew Moraes <andrewmoraesds@gmail.com>
2024-06-15 18:21:08 -0400

    add tasks section and model

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
+import { Task } from '../shared/models/task.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  taskInput: string = '';
 
+  //TODO: convert DATE to localtime
+  tasks: Task[] = [
+    {id:1, done: false, text: 'Teste 01', createdAt: new Date()},
+    {id:2, done: true, text: 'Teste 02', createdAt: new Date()},
+    {id:3, done: false, text: 'Teste 03', createdAt: new Date()}
+  ]
+
+  checkTask(task: Task) {
+    task.done = !task.done;
+  }
+  
+  addTask(text: string) {
+    if(text)
+      this.tasks.push({id: this.tasks.length + 1, text, done: false, createdAt: new Date()})
+
+    this.taskInput = '';
+  }
 }
